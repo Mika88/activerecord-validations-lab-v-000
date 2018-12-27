f97d93c629ef24fed1b73a8439fae35b546bff3c
Mika88 <mikaspivak@gmail.com>
2018-12-27 23:24:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,16 @@
 class Post < ActiveRecord::Base
-  @clickbait_arr = ["Won't Believe", "Secret", "Top", "Guess"]
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :clickbait
 
+  clickbait_arr = ["Won't Believe", "Secret", "Top", "Guess"]
+
   def clickbait
     if title
           binding.pry
-      @clickbait_arr.any?{|word| word.match title}
+      clickbait_arr.any?{|word| word.match title}
 
     end
   end
